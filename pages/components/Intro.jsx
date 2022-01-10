5e08e489018e0ec7d131aa07c0ff6644067ad318
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-10 16:35:22 +0530

    s5

---
+++ pages/components/Intro.jsx
@@ -0,0 +1,26 @@
+const Intro = () => {
+    return (
+        <>
+            <div className="intro">
+                <div className="container">
+                    <div className="box">
+                        <div className="left">
+                            <h3>Hello, My name is</h3>
+                            <h1>Arnab Saha</h1>
+                            <div className="title">
+                                <div className="titleItem">Web Developer</div>
+                                <div className="titleItem">Blockchain Developer</div>
+                                <div className="titleItem">Forntend Developer</div>
+                                <div className="titleItem">Backend Developer</div>
+                                <div className="titleItem">UI/UX Designer</div>
+                            </div>
+                            <p>I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Intro;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
+import Intro from './components/Intro'
 const index = () => {
   return (
     <>
       <Head>
         <title>TAS Portfolio-1</title>
       </Head>
-      arnab
+      <Intro/>
     </>
   );
 }
