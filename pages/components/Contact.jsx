18518c0cb4cfd3cd584dd2611a836ebc97ba8e7c
thearnabsaha <thearnabsaha201@gmail.com>
2022-01-12 14:12:04 +0530

    s8

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -1,7 +1,12 @@
 const Contact = () => {
     return (
         <>
-            
+            <div className="contact">
+                <div className="left">
+                    <h1>Let's discuss your project</h1>
+                    .
+                </div>
+            </div>
         </>
     );
 }
