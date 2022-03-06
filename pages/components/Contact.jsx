35669f4723baa9fa582c1da2698e0af56fe6760a
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-06 09:20:12 +0530

    s8

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -2,7 +2,21 @@ import Image from 'next/image';
 import phone from '../../public/img/phone.png';
 import email from '../../public/img/email.png';
 import address from '../../public/img/address.png';
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
 const Contact = () => {
+    const form = useRef();
+    const [done, setDone] = useState(false)
+    const handleSubmit =(e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_qrac7ua', 'template_r6sex1o', form.current, 'user_SvxI195VxTx62CejHs17q')
+        .then((result) => {
+            console.log(result.text);
+            setDone(true)
+        }, (error) => {
+            console.log(error.text);
+        });
+    }
     return (
         <>
             <div className="contact">
@@ -24,12 +38,12 @@ const Contact = () => {
                 </div>
                 <div className="right">
                     <p><span>What’s your story?</span> Get in touch. Always available for freelancing if the right project comes along. me.</p>
-                    <form action="">
-                        <input type="text" placeholder='Name'/>
-                        <input type="text" placeholder='Subject'/>
-                        <input type="email" placeholder='Email'/>
-                        <textarea placeholder='message'></textarea>
-                        <button type="submit">Submit</button>
+                    <form ref={form} onSubmit={handleSubmit}>
+                        <input type="text" placeholder='Name' name="user_name"/>
+                        <input type="text" placeholder='Subject' name="user_subject"/>
+                        <input type="email" placeholder='Email' name="user_email"/>
+                        <textarea placeholder='message' name="message"></textarea>
+                        <button type='submit' value="send">Submit</button>
                     </form>
                 </div>
             </div>
